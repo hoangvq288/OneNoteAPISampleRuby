cffda5381a81edf9aaf18a73864d8f397e4e2ef2
Eric Vu <eric.vu@inspireventures.com>
2018-10-02 15:12:41 +0700

    Improve code style for Welcome Controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -13,22 +13,18 @@ class WelcomeController < ApplicationController
     @response_code = nil
     onenote_client = OneNoteSharer.new
     access_token = cookies['access_token']
-    result = nil
-
-    case params[:submit]
-      when 'text'
-        result = onenote_client.create_page_with_simple_text(access_token)
-      when 'textimage'
-        result = onenote_client.create_page_with_text_and_image(access_token)
-      when 'url'
-        result = onenote_client.create_page_with_screenshot_from_url(access_token)
-      when 'html'
-        result = onenote_client.create_page_with_screenshot_from_html(access_token)
-      when 'file'
-
-        result = onenote_client.create_page_with_file(access_token)
-    end
-	
+    result = case params[:submit]
+             when 'text'
+               onenote_client.create_page_with_simple_text(access_token)
+             when 'textimage'
+               onenote_client.create_page_with_text_and_image(access_token)
+             when 'url'
+               onenote_client.create_page_with_screenshot_from_url(access_token)
+             when 'html'
+               onenote_client.create_page_with_screenshot_from_html(access_token)
+             when 'file'
+               onenote_client.create_page_with_file(access_token)
+             end
     @oneNoteWeb_url = result['links']['oneNoteWebUrl']['href'] if result.present?
 
   rescue Exception => e
@@ -39,15 +35,13 @@ class WelcomeController < ApplicationController
   def callback
     onenote_client = OneNoteSharer.new
     token_set = onenote_client.handle_callback_request(params)
-    if token_set.present?
-      expire_in = token_set['expires_in'].to_i
-      cookies['access_token'] = { :value => token_set['access_token'], :expires => expire_in.seconds.from_now }
-      cookies['authentication_token'] = { :value => token_set['authentication_token'], :expires => expire_in.seconds.from_now }
-      cookies['scope'] = { :value => token_set['scope'], :expires => expire_in.seconds.from_now }
-      refresh_token = token_set['refresh_token']
-
-      onenote_client.save_refresh_token(refresh_token) if refresh_token.present?
-    end
+    return unless token.present?
+    expire_in = token_set['expires_in'].to_i
+    cookies['access_token'] = { :value => token_set['access_token'], :expires => expire_in.seconds.from_now }
+    cookies['authentication_token'] = { :value => token_set['authentication_token'], :expires => expire_in.seconds.from_now }
+    cookies['scope'] = { :value => token_set['scope'], :expires => expire_in.seconds.from_now }
+    refresh_token = token_set['refresh_token']
+    onenote_client.save_refresh_token(refresh_token) if refresh_token.present?
   end
 
 end
